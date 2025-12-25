f738b98218d407aafe1fa126e3b2d42a5a4c7959
Nguyễn Ngọc Tuyền <tt98tuyen@gmail.com>
2025-12-25 11:30:03 +0700

    fix: update users controller route to remove 'api/' prefix

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -20,7 +20,7 @@ import { RolesGuard } from '../auth/guards/roles.guard';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { UserRole, UserStatus } from './entities/user.entity';
 
-@Controller('api/users')
+@Controller('users')
 @UseInterceptors(ClassSerializerInterceptor)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
